50ad3d23ff4d39001276faf237b2cd0c03a24dd9
vorsanigo <verorsanigo@gmail.com>
2021-01-21 17:20:24 +0100

    Update copy_cleaning_1.py
    file that substitutes cleaning_1, added description functions

--- copy_cleaning_1.py
+++ copy_cleaning_1.py
@@ -1,17 +1,3 @@
-# CLEANING FROM https://towardsdatascience.com/basic-tweet-preprocessing-in-python-efd8360d529e
-# AND https://www.kaggle.com/ragnisah/text-data-cleaning-tweets-analysis
-
-# TODO NB !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-# TODO output -> DATASET_covid19_tweets_cleaned_NOhashtags.csv -> with list, string, tuples cleaned text and NO hashtags (in a separated column)
-# TODO output -> DATASET_covid19_tweets_cleaned_NOhashtags.csv -> with list, string, tuples cleaned text and YES hashtags
-
-# TODO this second cleaning file (cleaning_1.py) seems better then cleaning_NOT_correct.py
-
-# TODO choose if we want stemming (probably better avoid lemmatization), now we used stemming in this code
-# STEMMING: Working -> Work ___ it eliminates affixes from a word (stemming is faster)
-# LEMMATIZATION: Better -> Good ___ it uses vocabulary and morphological analysis of words to detect the lemma of the word (basic form)
-
 import pandas as pd
 import string
 import re
@@ -29,13 +15,21 @@ from nltk.tokenize import TweetTokenizer
 import pickle
 from datetime import datetime
 
-# Different functions to have the cleaned text in different possible forms
+
+# CLEANING FROM https://towardsdatascience.com/basic-tweet-preprocessing-in-python-efd8360d529e
+# AND https://www.kaggle.com/ragnisah/text-data-cleaning-tweets-analysis
+
+# STEMMING: Working -> Work ___ it eliminates affixes from a word (stemming is faster)
+# LEMMATIZATION: Better -> Good ___ it uses vocabulary and morphological analysis of words to detect the lemma of the word (basic form)
 
 ps = nltk.PorterStemmer()
 stopword = nltk.corpus.stopwords.words('english')
 
-# Clean and put the text into a list
+# Different functions to have the cleaned text in different possible forms
+
 def clean_text(text):
+    '''Given a text (string), it removes punctuation, numbers, it does tokenization, stemming, and removes stopwords,
+    the output is a list of tokens'''
     text_lc = "".join([word.lower() for word in text if word not in string.punctuation]) # remove puntuation
     text_rc = re.sub('[0-9]+', '', text_lc)
     tokens = re.split('\W+', text_rc)    # tokenization
@@ -44,8 +38,9 @@ def clean_text(text):
     #text = [word for word in tokens if word not in stopword]  # remove stopwords and stemming
     return text
 
-# Clean and put the text into a string
 def clean_text_string(text):
+    '''Given a text (string), it removes punctuation, numbers, it does tokenization, stemming, and removes stopwords,
+    the output is a string of tokens'''
     text_lc = "".join([word.lower() for word in text if word not in string.punctuation]) # remove puntuation
     text_rc = re.sub('[0-9]+', '', text_lc)
     tokens = re.split('\W+', text_rc)    # tokenization
@@ -60,8 +55,9 @@ def clean_text_string(text):
             text = text + " " + ps.stem(word) # remove stopwords and stemming
     return text
 
-# Clean and put the text into a tuple
 def clean_text_tuple(text):
+    '''Given a text (string), it removes punctuation, numbers, it does tokenization, stemming, and removes stopwords,
+    the output is a tuple of tokens'''
     text_lc = "".join([word.lower() for word in text if word not in string.punctuation]) # remove puntuation
     text_rc = re.sub('[0-9]+', '', text_lc)
     tokens = re.split('\W+', text_rc)    # tokenization
@@ -71,41 +67,28 @@ def clean_text_tuple(text):
     text_tuple = tuple(text)
     return text_tuple
 
-# TODO not used ????????????? used later
 def to_date(date_time):
+    '''Given a date_time, it returns a date'''
     # split following datetime format in the dataset
     date_time_obj = datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S')
     # date from datetime object
     date = date_time_obj.date()
     return date
 
-# cleaning function
 def cleaning(df_path, list_column_drop, path_cleaned, path_grouped):
+    '''Given a dataset of tweets, it cleans tweets' text and it groups the cleaned tweets by date'''
 
     # read the starting dataset
     df = pd.read_csv(df_path)
-    #df = pd.read_csv("/home/veror/PycharmProjects/DataMiningProj_OK/DATASET_covid19_tweets.csv")
-    #print(df.iloc[65503])
 
-    # keep only columns "date" and "text" and "hashtags"
+    # keep only columns selected ("date", "text", and "hashtags")
     df.drop(list_column_drop, axis=1, inplace=True)
     #df.drop(['user_name', 'user_location', 'user_description', 'user_created', 'user_followers', 'user_friends',
     #         'user_favourites', 'user_verified', 'source', 'is_retweet'], axis=1, inplace=True)
 
-    #print(df.iloc[65503])
-    #print(df)
-    #df.to_csv(r'/home/veror/PycharmProjects/DataMiningProj_OK/PROVA1.csv', index=False)
     #segmenter using the word statistics from Twitter
     # TODO PROBABLY TO ELIMINATE
     seg_tw = Segmenter(corpus="twitter")
-
-    # PROBABLY NOT USEFUL
-    #forming a separate feature for cleaned tweets
-    #print(enumerate(df['text']))
-    #for i,v in enumerate(df['text']):
-        #df.loc[v,'text'] = p.clean(i)
-        #print(i)
-        #print(df.loc[v,'text'])
 
     # CLEANING
 
@@ -114,19 +97,15 @@ def cleaning(df_path, list_column_drop, path_cleaned, path_grouped):
     p.set_options(p.OPT.URL, p.OPT.MENTION, p.OPT.RESERVED, p.OPT.EMOJI, p.OPT.SMILEY, p.OPT.NUMBER) # keep hashtags
     df['tweet_preprocessed'] = df['text'].apply(lambda x: p.clean(x))
 
-    #print(df)
-    #df.to_csv(r'/home/veror/PycharmProjects/DataMiningProj_OK/PROVA.csv', index=False)
-    #print(df.iloc[65503])
-
     # step 2: apply the cleaning functions on the preprocessed tweets and save results into new columns in the dataframe
     df['text_cleaned'] = df['tweet_preprocessed'].apply(lambda x: clean_text(x))
     df['text_cleaned_string'] = df['tweet_preprocessed'].apply(lambda x: clean_text_string(x))
     df['text_cleaned_tuple'] = df['tweet_preprocessed'].apply(lambda x: clean_text_tuple(x))
+
     # create column containing only the date, without the time
     df['date_only'] = df['date'].apply(lambda x: to_date(x))
 
-    # list of hashtags added to a new column -> don't know if it is useful -> NO we already have hashtags from the starting dataset
-    # DECOMMENT/COMMENT THIS LINE TO REMOVE/KEEP HASHTAGS
+    # alternative manual way to remove hashtags
     # df['hashtags'] = df['text'].apply(lambda x: re.findall(r"#(\w+)", x))
 
     # create the datsets grouped by date -> we only use tuple
@@ -135,14 +114,14 @@ def cleaning(df_path, list_column_drop, path_cleaned, path_grouped):
     df3 = pd.DataFrame(df.groupby('date_only')['text_cleaned_tuple'].apply(list).reset_index())
 
     # save the datasets created into csv
-    # SEPARATORE E' ' ' !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # separator is ' '
     df.to_csv(path_cleaned, index=False, sep=' ', line_terminator='\n')
-    '''df.to_csv(r'/home/veror/PycharmProjects/DataMiningProj_OK/DATASET_covid19_tweets_cleaned_PR.csv', index=False,
+    '''df.to_csv(r'/home/veror/PycharmProjects/DataMiningProj_OK/DATASET_covid19_tweets_cleaned.csv', index=False,
               sep=' ', line_terminator='\n')'''
     #df1.to_csv(r'/home/veror/PycharmProjects/DataMiningProj_OK/DATASET_covid19_group_text.csv', index=False)
     #df2.to_csv(r'/home/veror/PycharmProjects/DataMiningProj_OK/DATASET_covid19_group_string.csv', index=False)
     df3.to_csv(path_grouped, index=False, sep=' ')
-    df3.to_csv(r'/home/veror/PycharmProjects/DataMiningProj_OK/DATASET_covid19_group_tuple.csv', index=False, sep=' ')
+    #df3.to_csv(r'/home/veror/PycharmProjects/DataMiningProj_OK/DATASET_covid19_group_tuple.csv', index=False, sep=' ')
     #df3.to_csv(r'/home/veror/PycharmProjects/DataMiningProj_OK/DATASET_covid19_group_tuple_sep_comma.csv', index=False)
 
 cleaning("/home/veror/PycharmProjects/DataMiningProj_OK/DATASET_covid19_tweets.csv", ['user_name', 'user_location',
