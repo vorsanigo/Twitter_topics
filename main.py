9347eda40c6c3581f7d0bff48370d38b5cb22f0c
vorsanigo <verorsanigo@gmail.com>
2021-01-21 11:48:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
+import algorithm
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# TODO per semplicità lasciare i dataset possibili nella stessa cartella, così non necessita il path
+# accetta sia .csv che senza perché lo aggiunge lui
+def input_user():
+    dataset = input("Select the dataset where you want to find frequent topics:")
+    if len(dataset) >= 4 and not (dataset[-4:] == '.csv'):
+        dataset = dataset + '.csv'
+    df = pd.read_csv(dataset, sep=' ')
+    print(df)
+    topics_number = int(input("\nSelect how many frequent topics you want are returned, type 0 if you want all the possible ones:"))
+    topics_length = int(input("\nSelect:\n1 - if you do NOT want topics with only one term\n2 - otherwise\nType:"))
+    type_algorithm = int(input("\nSelect the number of the algorithm you want to apply:\n1 - basic algorithm\n2 - developed algorithm\nType:"))
+    return dataset, topics_number, topics_length, type_algorithm
+res = input_user()
 
+def run_algorithm():
+    input_res = input_user()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#def algorithm_execution():
