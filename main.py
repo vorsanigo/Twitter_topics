f8fb042972bc5ec74fa7b85041adb9d77b71077a
vorsanigo <verorsanigo@gmail.com>
2021-01-22 16:39:01 +0100

    Update main.py
    updated

--- main.py
+++ main.py
@@ -16,26 +16,36 @@ import time
 # accetta sia .csv che senza perche lo aggiunge lui
 def input_user():
     '''Function to get parameters from the user to execute the algorithm to find frequent topics'''
+
+    topics_singletons = 2
+    type_algorithm = 3
+
     dataset = input("Select the dataset where you want to find frequent topics:")
     if len(dataset) >= 4 and not (dataset[-4:] == '.csv'):
         dataset = dataset + '.csv'
-    df = pd.read_csv(dataset, sep=' ')
-    print(df)
-    topics_singletons = 2
-    type_algorithm = 3
+    #df = pd.read_csv(dataset, sep=' ')
+    #print(df)
+    print("\nDATASET ACQUIRED")
+
     topics_number = int(input("\nSelect how many frequent topics you want are returned type 0 if you want all the possible ones:"))
+
     while(type_algorithm != 0 and type_algorithm != 1 and type_algorithm != 2):
         type_algorithm = int(input("\nSelect the number of the algorithm you want to apply:\n0 - baseline algorithm\n1 - apriori-based algorithm\n2 - apriori-based algorithms with association rules\nType:"))
+
     if type_algorithm != 2:
         while (topics_singletons != 0 and topics_singletons != 1):
             topics_singletons = int(input("\nSelect:\n0 - if you do NOT want topics with only one term\n1 - if you want also topics with only one term\nType:"))
+
     return {'dataset': dataset, 'topics_number': topics_number, 'topics_singletons': topics_singletons, 'type_algorithm': type_algorithm}
 #res = input_user()
+
 
 def run_algorithm():
     '''Function to run the algorithm to find frequent topics, it return the output as dataframe and it saves it as csv,
     where topics are sorted by the number of days in which they appear on the overall period of time'''
+
     input_res = input_user()
+
     df_grouped = pd.read_csv(input_res['dataset'], sep=' ') # dataset
     column_dataframe = df_grouped['text_cleaned_tuple']
     list_date = df_grouped['date_only'].tolist()
@@ -45,7 +55,10 @@ def run_algorithm():
     #print(num_date)
     #print(num_date[2])
     #if input_res['topics_singletons'] == 1: # yes singletons
+
+    print("\nSTART SEARCHING FOR TOPICS")
     start_time = time.time()
+
     if input_res['type_algorithm'] == 0:
         # run naive
         apply_fun_res = algorithm.apply_fun('naive_fun_freq', num_date, column_dataframe,
@@ -56,6 +69,7 @@ def run_algorithm():
     else:
         # run eff_apriori_rules
         apply_fun_res = algorithm.apply_fun('eff_apriori_rules_fun', num_date, column_dataframe, input_res['topics_singletons'])
+
     # compute result
     res = algorithm.create_dict_topics(apply_fun_res)
     # create dataframe of results
@@ -63,14 +77,18 @@ def run_algorithm():
     df_topics["Number_of_occurrences"] = res[2]
     # sort dataframe of results by descendent frequence of topics
     sorted_df_topics = df_topics.sort_values(by='Number_of_occurrences', ascending=False)
+    # cut results according to user's request
     if not (input_res['topics_number'] == 0):
         result = sorted_df_topics.head(input_res['topics_number'])
     else:
         result = sorted_df_topics
-    print('Time to find frequent topics:')
+
+    print('\nTime to find frequent topics:')
     time_topics = time.time() - start_time
     print("--- %s seconds ---" % time_topics)
-    # cut results according to user's request
+    print("\n")
+
+    # save result into folder results
     if input_res['type_algorithm'] == 0:
         result.to_csv('results/algorithm_result_naive.csv', sep=' ')
         f = open("results/time_naive.txt", 'w')
@@ -88,8 +106,9 @@ def run_algorithm():
         f.close()
     return result
 
+# RUN PROGRAM
 print(run_algorithm())
-
+print("\nTHE OUTPUT IS IN THE FOLDER bin/results")
 
 
 # SORT BY FREQUENCE
