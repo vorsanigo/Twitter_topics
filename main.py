0cc27bf8658e1fe15941835cb0e5313546755fca
vorsanigo <verorsanigo@gmail.com>
2021-01-23 15:25:00 +0100

    Update main.py
    refactoring

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ import algorithm
 import pandas as pd
 import time
 
+# TODO RESULTS SAVED WITH ' ' SEPARATOR IN APRIORI RESULTS ROW 100 OF THIS SCRIPT, BUT THE ONES IN RESULTS HAVE STILL ,
+#  -> IF WE RUN LOT_TOPICS WE NEED TO CHANGE SEP IN ROW 5 OF PLOT_TOPICS.PY
+
+
 
 # -*- coding: utf-8 -*-
 # TODO ESPERIMENTI CON FZ: APRIORI, APRIORI_RULES, NAIVE (FEQ O TOP_KEY?)
-# TODO GRAFICO DI QUALCUNO
+# TODO GRAFICO DI QUALCUNO -> OK
 # TODO CONSIDERARE QUELLI FREQUENTI SOLO IN UN GIORNO? MAGARI NO/SI -> ARGOMENTARE
 # TODO magari togliere parole super frequenti?
 # TODO QUALI ALTRI ESPERIMENTI FARE? ALTRO DATASET, SOTTOPORZIONI DI DATASET? ANALISI STATISTICHE?
 # TODO COME USARE L'UTENTE?
 # TODO REPORT
+
+
 
 # TODO per semplicita' lasciare i dataset possibili nella stessa cartella, cosi non necessita il path
 # accetta sia .csv che senza perche lo aggiunge lui
@@ -21,13 +27,13 @@ def input_user():
     type_algorithm = 3
 
     dataset = input("Select the dataset where you want to find frequent topics:")
-    if len(dataset) >= 4 and not (dataset[-4:] == '.csv'):
-        dataset = dataset + '.csv'
+    '''if len(dataset) >= 4 and not (dataset[-4:] == '.csv'):
+        dataset = dataset + '.csv'''
     #df = pd.read_csv(dataset, sep=' ')
     #print(df)
     print("\nDATASET ACQUIRED")
 
-    topics_number = int(input("\nSelect how many frequent topics you want are returned type 0 if you want all the possible ones:"))
+    topics_number = int(input("\nSelect how many frequent topics you want are returned, type 0 if you want all the possible ones:"))
 
     while(type_algorithm != 0 and type_algorithm != 1 and type_algorithm != 2):
         type_algorithm = int(input("\nSelect the number of the algorithm you want to apply:\n0 - baseline algorithm\n1 - apriori-based algorithm\n2 - apriori-based algorithms with association rules\nType:"))
@@ -46,7 +52,7 @@ def run_algorithm():
 
     input_res = input_user()
 
-    df_grouped = pd.read_csv(input_res['dataset'], sep=' ') # dataset
+    df_grouped = pd.read_pickle(input_res['dataset']) # dataset
     column_dataframe = df_grouped['text_cleaned_tuple']
     list_date = df_grouped['date_only'].tolist()
     #print(list_date)
@@ -56,7 +62,7 @@ def run_algorithm():
     #print(num_date[2])
     #if input_res['topics_singletons'] == 1: # yes singletons
 
-    print("\nSTART SEARCHING FOR TOPICS")
+    print("\n\nSTART SEARCHING FOR TOPICS\n")
     start_time = time.time()
 
     if input_res['type_algorithm'] == 0:
@@ -83,14 +89,14 @@ def run_algorithm():
     else:
         result = sorted_df_topics
 
-    print('\nTime to find frequent topics:')
+    print('\nTime to find frequent topics:\n')
     time_topics = time.time() - start_time
     print("--- %s seconds ---" % time_topics)
     print("\n")
 
     # save result into folder results
     if input_res['type_algorithm'] == 0:
-        result.to_csv('results/algorithm_result_naive.csv', sep=' ')
+        result.to_csv('results/algorithm_result_naive.csv', sep=',')
         f = open("results/time_naive.txt", 'w')
         f.write("Time naive: " + str(time_topics))
         f.close()
@@ -100,7 +106,7 @@ def run_algorithm():
         f.write("Time apriori: " + str(time_topics))
         f.close()
     else:
-        result.to_csv('results/algorithm_result_apriori_rules.csv', sep=' ')
+        result.to_csv('results/algorithm_result_apriori_rules.csv', sep=',')
         f = open("results/time_apriori_rules.txt", 'w')
         f.write("Time apriori rules: " + str(time_topics))
         f.close()
