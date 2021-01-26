de2d9f4f1fadb8654f911961a12b21291df8f542
vorsanigo <verorsanigo@gmail.com>
2021-01-26 20:55:19 +0100

    Update plot_topics.py
    updated new plot

--- plot_topics.py
+++ plot_topics.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import pre_analysis
 
-df_covid_grouped = pd.read_csv("data/covid_grouped.csv", sep=' ')
-covid_apriori_res = pd.read_csv("results/COVID apriori total_no_singletons/algorithm_result_apriori_NO_singleton.csv", sep=' ')
+'''df_covid_grouped = pd.read_csv("data/covid_grouped.csv", sep=' ')
+covid_apriori_res = pd.read_csv("results/COVID apriori total_no_singletons/algorithm_result_apriori_NO_singleton.csv", sep=',')
 list_date = df_covid_grouped['date_only'].tolist()
 #print(list_date)
 #print(covid_apriori_res)
@@ -12,4 +12,12 @@ pre_analysis.plot_freq_topic(list_date, covid_apriori_res, 11, 'data/plot_covid_
 # ('case', 'covid', 'new') -> index = 8
 pre_analysis.plot_freq_topic(list_date, covid_apriori_res, 8, 'data/plot_case_covid_new.png', '(case, covid, new)')
 # (covid, vaccin) -> index = 14
-pre_analysis.plot_freq_topic(list_date, covid_apriori_res, 14, 'data/plot_covid_vaccin_new.png', '(covid, vaccin)')
+pre_analysis.plot_freq_topic(list_date, covid_apriori_res, 14, 'data/plot_covid_vaccin_new.png', '(covid, vaccin)')'''
+
+
+df_australia_grouped = pd.read_csv("data/australia_grouped.csv", sep=' ')
+australia_apriori_res = pd.read_csv("/home/veror/PycharmProjects/DataMiningProj_OK/results/RESULTS support 0.015/RESULTS ON REPORT/AUSTRALIA/apriori rules/0.9.csv", sep=',')
+list_date = df_australia_grouped['date_only'].tolist()
+
+# (elect, morrison, scott) -> index = 2
+pre_analysis.plot_freq_topic(list_date, australia_apriori_res, 2, 'data/plot_australia_elect_morrison_scott.png', '(elect, morrison, scott)')
