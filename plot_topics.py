f1c3c676b69dd50e7e496b453b628d71641d6e16
vorsanigo <verorsanigo@gmail.com>
2021-01-27 09:06:15 +0100

    Update plot_topics.py
    refactoring

--- plot_topics.py
+++ plot_topics.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pre_analysis
 
-'''df_covid_grouped = pd.read_csv("data/covid_grouped.csv", sep=' ')
+df_covid_grouped = pd.read_csv("data/covid_grouped.csv", sep=' ')
 covid_apriori_res = pd.read_csv("results/COVID apriori total_no_singletons/algorithm_result_apriori_NO_singleton.csv", sep=',')
 list_date = df_covid_grouped['date_only'].tolist()
 #print(list_date)
@@ -12,7 +12,7 @@ pre_analysis.plot_freq_topic(list_date, covid_apriori_res, 11, 'data/plot_covid_
 # ('case', 'covid', 'new') -> index = 8
 pre_analysis.plot_freq_topic(list_date, covid_apriori_res, 8, 'data/plot_case_covid_new.png', '(case, covid, new)')
 # (covid, vaccin) -> index = 14
-pre_analysis.plot_freq_topic(list_date, covid_apriori_res, 14, 'data/plot_covid_vaccin_new.png', '(covid, vaccin)')'''
+pre_analysis.plot_freq_topic(list_date, covid_apriori_res, 14, 'data/plot_covid_vaccin_new.png', '(covid, vaccin)')
 
 
 df_australia_grouped = pd.read_csv("data/australia_grouped.csv", sep=' ')
