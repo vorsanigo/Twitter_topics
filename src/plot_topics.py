1fcc506523cf5455d533f04ef0d65dc0d8e1b550
vorsanigo <verorsanigo@gmail.com>
2021-01-27 18:22:20 +0100

    Create plot_topics.py
    moved

---
+++ src/plot_topics.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import pre_analysis
+
+
+# Script used to plot popular topics and their frequency: paths are the old ones, decomment and change paths to re-run
+
+'''
+# covid
+df_covid_grouped = pd.read_csv("../data/covid_grouped.csv", sep=' ')
+covid_apriori_res = pd.read_csv("results/COVID apriori total_no_singletons/algorithm_result_apriori_NO_singleton.csv", sep=',')
+list_date = df_covid_grouped['date_only'].tolist()
+
+# (covid, india) -> index = 11
+pre_analysis.plot_freq_topic(list_date, covid_apriori_res, 11, 'data/plot_covid_india.png', '(covid, india)')
+# ('case', 'covid', 'new') -> index = 8
+pre_analysis.plot_freq_topic(list_date, covid_apriori_res, 8, 'data/plot_case_covid_new.png', '(case, covid, new)')
+# (covid, vaccin) -> index = 14
+pre_analysis.plot_freq_topic(list_date, covid_apriori_res, 14, 'data/plot_covid_vaccin_new.png', '(covid, vaccin)')
+
+
+# australia
+df_australia_grouped = pd.read_csv("data/australia_grouped.csv", sep=' ')
+australia_apriori_res = pd.read_csv("/home/veror/PycharmProjects/DataMiningProj_OK/results/RESULTS support 0.015/RESULTS ON REPORT/AUSTRALIA/apriori rules/0.9.csv", sep=',')
+list_date = df_australia_grouped['date_only'].tolist()
+
+# (elect, morrison, scott) -> index = 2
+pre_analysis.plot_freq_topic(list_date, australia_apriori_res, 2, 'data/plot_australia_elect_morrison_scott.png', '(elect, morrison, scott)')
+'''
