4abd30c6ffb0d0bd24a5260d9d3840419077abfe
vorsanigo <verorsanigo@gmail.com>
2021-01-27 17:42:48 +0100

    Update pre_analysis.py
    refactoring

--- pre_analysis.py
+++ pre_analysis.py
@@ -2,16 +2,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pickle
 
+
+# Ausiliar functions for preanalysis of datasets and plots
+
+
 def tweets_day(df_grouped_path, text_col):
+    '''Given a dataset with tweets grouped by date, and the text column, it returns a list of dates and a list of the
+    number of tweets per day'''
     df_grouped = pd.read_csv(df_grouped_path, sep=' ')
     #print(df_grouped)
     counts = df_grouped[text_col].apply(lambda x: len(pd.eval(x)))
     list_counts = counts.to_list()
     list_date = df_grouped['date_only'].tolist()
     return list_date, list_counts
-#list_date_counts = tweets_day("/home/veror/PycharmProjects/DataMiningProj_OK/DATASET_covid19_group_tuple.csv", 'text_cleaned_tuple')
+
 
 def plot_date_tweets(list_date_counts, path_figure):
+    '''GIven the result of tweets_day, it plots the number of tweets per day in a barplot'''
     # barplot date - number of tweets
     plt.bar(list_date_counts[0], height=list_date_counts[1])
     plt.xticks(list_date_counts[0], rotation='vertical')
@@ -21,7 +28,9 @@ def plot_date_tweets(list_date_counts, path_figure):
     plt.savefig(path_figure)
     plt.show()
 
+
 def plot_freq_topic(list_date, res_df, topic_index, path_figure, title_plot):
+    '''Given a popular topic over time and the days in which it is popular, it plots its frequency for each day'''
     list_freq = []
     list_date_ok = []
     for column in list_date:
